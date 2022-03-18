f40577c34adbaf42498b635840593e984c1779df
Leonid Negashev <leonid.negaschev@gmail.com>
2022-03-18 19:28:23 +0500

    ref(): add print in console

--- new_code.py
+++ new_code.py
@@ -3,11 +3,13 @@ def check_palindrome(a):
 
 def palindrome_chain_length(n):
     steps=0
+    print(n)
     while(check_palindrome(n)!=True):
         n=n+int(str(n)[::-1])
         steps+=1
     return steps
 
-if name == 'main':
+
+if __name__ == '__main__':
     for n in range(1, 100):
-        palindrome_chain_length(n)
+        print(palindrome_chain_length(n))
